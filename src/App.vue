c3f16f2c420dc84417c5c1ae05cc8bc4d28ce362
Luiza Nunes <luizanunes.lnr@gmail.com>
2024-07-30 17:36:38 -0300

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div 
+      v-for="obj in todos"
+      v-bind:key="obj.id"
+      class="todos-item"
+    >
+      {{ index }} - {{ obj.title }}
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todos: [
+        {
+          userId: 1,
+          id: 1,
+          title: 'delectus aut autem',
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: 'quis ut nam facilis et officia qui',
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: 'fugiat veniam minus',
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 4,
+          title: 'et porro tempora',
+          completed: true,
+        },
+        {
+          userId: 1,
+          id: 5,
+          title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+          completed: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
+.todos-item {
+  background-color: black;
+  margin: 0 0 5px 0;
+  padding: 3px 6px;
+  color: white;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #2c3e50;
+  margin: 60px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,87 @@
+<template>
+    <div id="app">
+        
+        <h1 :class="classVar">
+            Curso Vue 3
+        </h1>
+
+        <div 
+            v-for="(obj, index) in todos"
+            :key="obj.id"
+            class="todos-item"
+        >
+            <img
+                v-if="obj.imgSrc"
+                :src="obj.imgSrc"
+                alt="Todo Image"
+            />
+            {{ index }} - {{ obj.title }}
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            classVar: 'title',
+            todos: [
+                {
+                    "userId": 1,
+                    "id": 1,
+                    "title": "delectus aut autem",
+                    "completed": false,
+                    "imgSrc": 'https://via.placeholder.com/150'
+                },
+                {
+                    "userId": 1,
+                    "id": 2,
+                    "title": "quis ut nam facilis et officia qui",
+                    "completed": false,
+                    "imgSrc": 'https://via.placeholder.com/150'
+                },
+                {
+                    "userId": 1,
+                    "id": 3,
+                    "title": "jiobi oyuhj yui yui hj",
+                    "completed": true
+                },
+                {
+                    "userId": 1,
+                    "id": 4,
+                    "title": "asdwasdws aw da",
+                    "completed": true
+                },
+                {
+                    "userId": 1,
+                    "id": 5,
+                    "title": "dghg dfgrey dfg",
+                    "completed": false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+.title {
+    font-size: 20px;
+    color: blue; /* Ajuste para garantir que a cor azul seja aplicada */
+}
+
+.todos-item {
+    margin-bottom: 10px;
+}
+</style>
