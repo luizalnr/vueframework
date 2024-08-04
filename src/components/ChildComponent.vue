234124d0af328d752be04f0358b31c07235ab4ef
Luiza Nunes <luizanunes.lnr@gmail.com>
2024-08-04 13:45:01 -0300

    vue

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <!--botao q emite evento ao ser clicado-->
+  <button @click="notifyParent">Notificar Pai</button>
+</template>
+
+
+
+<script>
+  export default {
+    methods: {
+      notifyParent() {
+        //evento customizavel com uma mensagem
+        this.$emit('custom-event', 'Mensagem do filho');
+      }
+    }
+  }
+</script>
---
+++ src/components/ParentComponent.vue.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <!--Botao chama metodo increment ao ser clicado-->
+        <button @click="increment">Incrementar</button>
+
+        <!--Mostra o valor do contator-->
+        <p>Contador: {{ count }}</p>
+
+        <!--Componente filho, passa um metodo para o evento customizado-->
+        <ChildComponent @custom-event="handleCustomEvent" />
+    </div>
+</template>
+
+
+
+
+<script>
+    //importa o componente filho
+    import ChildComponent from './ChildComponent.vue';
+
+    export default {
+        components: {
+            ChildComponent //declara o componente filho
+        },
+        data() {
+            return {
+                count: 0 //inicio do contator
+            };
+        },
+        methods: {
+            increment() {
+                this.count++; //ao clicar add +1 ao contador
+            },
+            handleCustomEvent(message) {
+                console.log(message); //mensagem do filho
+            }
+        }
+    }
+</script>
